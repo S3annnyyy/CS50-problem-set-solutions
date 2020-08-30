d12e4bffd1550597264f3b396803dce1cfa99ac1
Sean Yap <67400060+S3annnyyy@users.noreply.github.com>
2020-08-30 16:11:44 +0800

    Delete Pset 1 hello.c

--- Pset 1 hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <cs50.h>
-
-int main(void)
-{
-    // getting name input from user and printing it
-    string name = get_string("What is your name?\n");
-    printf("Hello, %s\n", name);
-}
