88c04c482c7c8376ada39e3421ab54259ab9abf2
Sean Yap <67400060+S3annnyyy@users.noreply.github.com>
2020-08-30 16:13:13 +0800

    Create Readability.c

---
+++ Pset 2/Readability.c
@@ -0,0 +1,60 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <math.h>
+
+int main(int argc, string argv[])
+{
+    //counting letters
+    string text = get_string("Text: ");
+    int k = strlen(text);                          //getting length of text in int
+    float letters_count = 0;
+    for (int i = 0; i < k; i++)                    //for loop to count no.of letters
+    {
+        if (isalpha(text[i]))
+        {
+            letters_count++;
+        }
+    }
+
+    //counting no.of words
+    float word_count = 0;
+    for (int w = 0; w < k; w++)
+    {
+        if(text[w] == ' ')
+        {
+            word_count++;
+        }
+    }
+     word_count++;
+
+    //counting no. of sentences
+    float sentence_count = 0;
+    for (int y = 0; y <k; y++)
+    {
+        if (text[y] == '!' || text[y] == '.' || text[y] == '?')
+        {
+            sentence_count++;
+        }
+    }
+
+    //finding L and S that is part of Coleman-Liau index
+    float L = (letters_count/word_count) * 100;
+    float S = (sentence_count/word_count) * 100;
+    float index = 0.0588 * L -0.296 * S -15.8;
+    int grade_number = round(index);
+
+    if (grade_number > 16)
+    {
+        printf("Grade 16+");
+    }
+    else if (grade_number < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else
+    {
+        printf("Grade %i\n", grade_number);
+    }
+}
